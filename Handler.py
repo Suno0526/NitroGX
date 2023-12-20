5b02eda4195574b599a15a7316544f49bd812e47
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2023-12-20 15:22:47 -0800

    💸 | Released OGX Nitro generator

--- Handler.py
+++ Handler.py
@@ -1,12 +1,17 @@
 import requests as Requests
 import time as Time
+import subprocess as Sub
+import os as OS
+
+Filed = OS.path.join(OS.path.dirname(__file__), "Codes.txt")
+Numero = int(input("Enter the number of Opera GX Nitro codes: "))
 
 with open("Codes.txt", "w") as File:
-    for _ in range(100):
+    for _ in range(Numero):
         URI = "https://api.discord.gx.games/v1/direct-fulfillment"
         Data = {"partnerUserId": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"}
 
-        Resp = Requests.post(URI, json = Data)
+        Resp = Requests.post(URI, json=Data)
         JSON = Resp.json()
 
         Token = JSON.get('token', '').replace("'", "")
@@ -14,4 +19,7 @@ with open("Codes.txt", "w") as File:
 
         Time.sleep(0.05)
 
-        print(Token)
+        print(f"https://discord.com/billing/partner-promotions/1180231712274387115/{Token}")
+
+
+Sub.Popen(["notepad.exe", Filed])
